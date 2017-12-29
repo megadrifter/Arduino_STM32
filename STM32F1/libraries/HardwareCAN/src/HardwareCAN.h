2b166f39a17f1d4fd2ab9a03a6bd140ee24da720
Lacklustr Labs <lacklustrlabs@gmail.com>
2017-12-29 17:47:50 +0100

    Removed some unused #defines

--- STM32F1/libraries/HardwareCAN/src/HardwareCAN.h
+++ STM32F1/libraries/HardwareCAN/src/HardwareCAN.h
@@ -8,16 +8,6 @@
 #include "utility/can.h"
 #include "usb_serial.h"
 
-#define PID_REQUEST		0x7DF
-#define PID_REPLY		0x7E8
-
-#define ENGINE_COOLANT_TEMP	0x05
-#define ENGINE_RPM			0x0C
-#define VEHICLE_SPEED		0x0D
-#define MAF_SENSOR			0x10
-#define O2_VOLTAGE			0x14
-#define THROTTLE			0x11
-
 /**
  * Defines the possible SPI communication speeds.
  */
